0029f8f82eb82877b339cb96a4f535053e139393
Mario Montero <marioupc3@gmail.com>
2022-04-25 21:28:18 -0500

    agregando graficas

---
+++ src/components/Chars/Doughnut.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ChartDataLabels);
+var options = {
+    tooltips: {
+        enabled: false
+    },
+    plugins: {
+        datalabels: {
+            formatter: (value, ctx) => {
+                let sum = 0;
+                let dataArr = ctx.chart.data.datasets[0].data;
+                dataArr.map(data => {
+                    sum += data;
+                });
+                let percentage = (value*100 / sum).toFixed(2)+"%";
+                return percentage;
+            },
+            color: '#fff',
+        }
+    }
+};
+export const DoughnutChart = ({ datachart }) => {
+    return (
+        <Doughnut options={options} data={datachart} />
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,10 +41,12 @@ import team4 from "../assets/images/team-4.jpg";
 import card from "../assets/images/info-card-1.jpg";
 import { UseApiService } from "../Services/useApiServices";
 import { MonthsSpanish } from "../Services/Monts";
+import { DoughnutChart } from "../components/Chars/Doughnut";
+import { UseTypeDataChart } from "../Hooks/useTypeDataChart";
 
 function Home() {
   const { Title, Text } = Typography;
- 
+  const [ carBrands, setcarBrands ] = useState([]);
   const [ counts, setcounts ] = useState([
     {
       today: "Total conductores LATAM",
@@ -79,7 +81,8 @@ function Home() {
 
     UseApiService('get', {}, '/stats/basic/BASIC_STATS').then((res) => {
       console.log("🚀 ~ file: Home.js ~ line 55 ~ useApiService ~ res", res)
-      let { drivers, passengers } = res.data.stats;
+      let { drivers, passengers, carBrands } = res.data.stats;
+      setcarBrands(carBrands)
       setcounts([
         {
           today: "Total conductores LATAM",
@@ -113,179 +116,179 @@ function Home() {
     })
   }, [])
 
-  const onChange = (e) => console.log(`radio checked:${e.target.value}`);
+  // const onChange = (e) => console.log(`radio checked:${e.target.value}`);
 
-  const [ reverse, setReverse ] = useState(false);
+  // const [ reverse, setReverse ] = useState(false);
 
-  const list = [
-    {
-      img: ava1,
-      Title: "Soft UI Shopify Version",
-      bud: "$14,000",
-      progress: <Progress percent={60} size="small" />,
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team2} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Alexander Smith">
-            <img className="tootip-img" src={team3} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Jessica Doe">
-            <img className="tootip-img" src={team4} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
-    {
-      img: ava2,
-      Title: "Progress Track",
-      bud: "$3,000",
-      progress: <Progress percent={10} size="small" />,
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team2} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
-    {
-      img: ava3,
-      Title: "Fix Platform Errors",
-      bud: "Not Set",
-      progress: <Progress percent={100} size="small" status="active" />,
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Alexander Smith">
-            <img className="tootip-img" src={team3} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
-    {
-      img: ava4,
-      Title: "Launch new Mobile App",
-      bud: "$20,600",
-      progress: <Progress percent={100} size="small" status="active" />,
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team2} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
-    {
-      img: ava5,
-      Title: "Add the New Landing Page",
-      bud: "$4,000",
-      progress: <Progress percent={80} size="small" />,
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team2} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Alexander Smith">
-            <img className="tootip-img" src={team3} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Jessica Doe">
-            <img className="tootip-img" src={team4} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
+  // const list = [
+  //   {
+  //     img: ava1,
+  //     Title: "Soft UI Shopify Version",
+  //     bud: "$14,000",
+  //     progress: <Progress percent={60} size="small" />,
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team2} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Alexander Smith">
+  //           <img className="tootip-img" src={team3} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Jessica Doe">
+  //           <img className="tootip-img" src={team4} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     img: ava2,
+  //     Title: "Progress Track",
+  //     bud: "$3,000",
+  //     progress: <Progress percent={10} size="small" />,
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team2} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     img: ava3,
+  //     Title: "Fix Platform Errors",
+  //     bud: "Not Set",
+  //     progress: <Progress percent={100} size="small" status="active" />,
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Alexander Smith">
+  //           <img className="tootip-img" src={team3} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     img: ava4,
+  //     Title: "Launch new Mobile App",
+  //     bud: "$20,600",
+  //     progress: <Progress percent={100} size="small" status="active" />,
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team2} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     img: ava5,
+  //     Title: "Add the New Landing Page",
+  //     bud: "$4,000",
+  //     progress: <Progress percent={80} size="small" />,
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team2} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Alexander Smith">
+  //           <img className="tootip-img" src={team3} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Jessica Doe">
+  //           <img className="tootip-img" src={team4} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
 
-    {
-      img: ava6,
-      Title: "Redesign Online Store",
-      bud: "$2,000",
-      progress: (
-        <Progress
-          percent={100}
-          size="small"
-          status="exception"
-          format={() => "Cancel"}
-        />
-      ),
-      member: (
-        <div className="avatar-group mt-2">
-          <Tooltip placement="bottom" title="Ryan Tompson">
-            <img className="tootip-img" src={team1} alt="" />
-          </Tooltip>
-          <Tooltip placement="bottom" title="Romina Hadid">
-            <img className="tootip-img" src={team2} alt="" />
-          </Tooltip>
-        </div>
-      ),
-    },
-  ];
+  //   {
+  //     img: ava6,
+  //     Title: "Redesign Online Store",
+  //     bud: "$2,000",
+  //     progress: (
+  //       <Progress
+  //         percent={100}
+  //         size="small"
+  //         status="exception"
+  //         format={() => "Cancel"}
+  //       />
+  //     ),
+  //     member: (
+  //       <div className="avatar-group mt-2">
+  //         <Tooltip placement="bottom" title="Ryan Tompson">
+  //           <img className="tootip-img" src={team1} alt="" />
+  //         </Tooltip>
+  //         <Tooltip placement="bottom" title="Romina Hadid">
+  //           <img className="tootip-img" src={team2} alt="" />
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  // ];
 
-  const timelineList = [
-    {
-      title: "$2,400 - Redesign store",
-      time: "09 JUN 7:20 PM",
-      color: "green",
-    },
-    {
-      title: "New order #3654323",
-      time: "08 JUN 12:20 PM",
-      color: "green",
-    },
-    {
-      title: "Company server payments",
-      time: "04 JUN 3:10 PM",
-    },
-    {
-      title: "New card added for order #4826321",
-      time: "02 JUN 2:45 PM",
-    },
-    {
-      title: "Unlock folders for development",
-      time: "18 MAY 1:30 PM",
-    },
-    {
-      title: "New order #46282344",
-      time: "14 MAY 3:30 PM",
-      color: "gray",
-    },
-  ];
+  // const timelineList = [
+  //   {
+  //     title: "$2,400 - Redesign store",
+  //     time: "09 JUN 7:20 PM",
+  //     color: "green",
+  //   },
+  //   {
+  //     title: "New order #3654323",
+  //     time: "08 JUN 12:20 PM",
+  //     color: "green",
+  //   },
+  //   {
+  //     title: "Company server payments",
+  //     time: "04 JUN 3:10 PM",
+  //   },
+  //   {
+  //     title: "New card added for order #4826321",
+  //     time: "02 JUN 2:45 PM",
+  //   },
+  //   {
+  //     title: "Unlock folders for development",
+  //     time: "18 MAY 1:30 PM",
+  //   },
+  //   {
+  //     title: "New order #46282344",
+  //     time: "14 MAY 3:30 PM",
+  //     color: "gray",
+  //   },
+  // ];
 
-  const uploadProps = {
-    name: "file",
-    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-    headers: {
-      authorization: "authorization-text",
-    },
-    onChange(info) {
-      if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === "done") {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
+  // const uploadProps = {
+  //   name: "file",
+  //   action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  //   headers: {
+  //     authorization: "authorization-text",
+  //   },
+  //   onChange(info) {
+  //     if (info.file.status !== "uploading") {
+  //       console.log(info.file, info.fileList);
+  //     }
+  //     if (info.file.status === "done") {
+  //       message.success(`${info.file.name} file uploaded successfully`);
+  //     } else if (info.file.status === "error") {
+  //       message.error(`${info.file.name} file upload failed.`);
+  //     }
+  //   },
+  // };
 
   return (
     <>
@@ -322,8 +325,18 @@ function Home() {
 
         <Row gutter={[ 24, 0 ]}>
           <Col xs={24} sm={24} md={12} lg={12} xl={10} className="mb-24">
-            <Card bordered={false} className="criclebox h-full">
-              <Echart />
+            <Card
+              bordered hoverable className="criclebox h-full"
+              title={
+                <span style={{
+                  fontWeight: "bold",
+                  fontSize: "18px",
+                }}>
+                   Distribución de vehículos por marca
+                </span>
+              }>
+              {/* <Echart /> */}
+              <DoughnutChart datachart={UseTypeDataChart('car-brands', carBrands)} type="car-brands" />
             </Card>
           </Col>
           <Col xs={24} sm={24} md={12} lg={12} xl={14} className="mb-24">
@@ -333,7 +346,7 @@ function Home() {
           </Col>
         </Row>
 
-        <Row gutter={[ 24, 0 ]}>
+        {/* <Row gutter={[ 24, 0 ]}>
           <Col xs={24} sm={24} md={12} lg={12} xl={16} className="mb-24">
             <Card bordered={false} className="criclebox cardbody h-full">
               <div className="project-ant">
@@ -433,9 +446,9 @@ function Home() {
               </div>
             </Card>
           </Col>
-        </Row>
+        </Row> */}
 
-        <Row gutter={[ 24, 0 ]}>
+        {/* <Row gutter={[ 24, 0 ]}>
           <Col xs={24} md={12} sm={24} lg={12} xl={14} className="mb-24">
             <Card bordered={false} className="criclebox h-full">
               <Row gutter>
@@ -499,7 +512,7 @@ function Home() {
               </div>
             </Card>
           </Col>
-        </Row>
+        </Row> */}
       </div>
     </>
   );
